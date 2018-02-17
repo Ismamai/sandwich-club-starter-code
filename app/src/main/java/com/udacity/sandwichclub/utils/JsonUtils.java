c086cca62bfcb6efda594f8c923089043dc3d35d
iblesa <isma.work@gmail.com>
2018-02-17 10:06:29 +0000

    Use JSONObject to parse json string.

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,54 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
+    private static final String DESCRIPTION = "description";
+    private static final String NAME = "name";
+    private static final String MAIN_NAME = "mainName";
+    private static final String ALSO_KOWN_AS = "alsoKnownAs";
+    private static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    private static final String IMAGE = "image";
+    private static final String INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich result = new Sandwich();
+
+        try {
+            JSONObject object = new JSONObject(json);
+            JSONObject nameJsonObject = object.getJSONObject(NAME);
+            result.setMainName(nameJsonObject.getString(MAIN_NAME));
+            JSONArray alsoKnownAs = nameJsonObject.getJSONArray(ALSO_KOWN_AS);
+            result.setAlsoKnownAs(getList(alsoKnownAs));
+            JSONArray ingredients = object.getJSONArray(INGREDIENTS);
+            result.setIngredients(getList(ingredients));
+            result.setDescription(object.getString(DESCRIPTION));
+            result.setPlaceOfOrigin(object.getString(PLACE_OF_ORIGIN));
+            result.setImage(object.getString(IMAGE));
+            result.setDescription(object.getString(DESCRIPTION));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return result;
+    }
+
+    private static List<String> getList(JSONArray jsonArray) throws JSONException {
+        int count = 0;
+        List<String> list = new ArrayList<>(jsonArray.length());
+        while (count < jsonArray.length()) {
+            list.add(jsonArray.getString(count));
+            count++;
+        }
+        return list;
     }
 }
